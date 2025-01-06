567a5e017cbfec76db5d7cc20dbecd382054f9b3
Peter Francis Robante <robantepeterfran@gmail.com>
2025-01-06 22:53:18 +0800

    feat:dynamic user role in navbar

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -8,13 +8,13 @@
     </div>
 
 
-    <form id="profile-form" class="open-menu d-flex align-items-center justify-content-center gap-1" <p>Hello,
-        Admin!</p>
-        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
-            class="bi bi-person-circle" viewBox="0 0 16 16">
-            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
-            <path fill-rule="evenodd"
-                d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+    <form id="profile-form" class="open-menu d-flex align-items-center justify-content-center gap-1"
+        @if (auth()->user()->role === 'Admin') <p>Hello, Admin!</p> @endif
+        @if (auth()->user()->role === 'Personnel') <p>Hello, Personnel!</p> @endif <svg xmlns="http://www.w3.org/2000/svg"
+        width="30" height="30" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+        <path fill-rule="evenodd"
+            d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
         </svg>
     </form>
 </div>
@@ -148,7 +148,7 @@
                             ]; // Get the first error message
                             alert(
                                 errorMessage
-                                ); // Display error message (you may want to use a better method)
+                            ); // Display error message (you may want to use a better method)
                         }
                     } else {
                         alert('Failed to update password. Please try again.');
